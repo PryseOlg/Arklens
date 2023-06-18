9b53cfbaba932a20fd2422c8f79278e033bee1d9
PryseOlg <oleg.kuts.02@mail.ru>
2023-06-18 16:55:33 +0500

    update adapters

--- app/src/main/java/com/example/arklens/adapters/CharacterAdapter.kt
+++ app/src/main/java/com/example/arklens/adapters/CharacterAdapter.kt
@@ -1,4 +1,60 @@
 package com.example.arklens.adapters
 
-class CharacterAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.arklens.R
+import com.example.arklens.interfaces.CharacterListener
+import com.example.arklens.models.Character
+
+class CharacterAdapter(val listener: CharacterListener) :
+    ListAdapter<Character, RecyclerView.ViewHolder>(MyDiffCallback()) {
+
+    override fun getItemViewType(position: Int): Int {
+        return R.id.navigation_characters
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            R.id.navigation_characters -> {
+                val binding = CharacterBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent, false
+                )
+                CharacterHolder(parent.context, binding);
+            }
+
+            else -> throw IllegalStateException("Unknown view type $viewType")
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder.itemViewType) {
+            R.id.navigation_characters -> (holder as CharacterHolder).bind(getItem(position))
+            else -> throw IllegalStateException("Unknown item view type ${holder.itemViewType}")
+        }
+    }
+
+    class MyDiffCallback : DiffUtil.ItemCallback<Character>() {
+        override fun areItemsTheSame(
+            oldItem: Character,
+            newItem: Character
+        ): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(
+            oldItem: Character,
+            newItem: Character
+        ): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
 }
+
+
+
+
---
+++ app/src/main/java/com/example/arklens/models/Character.kt
@@ -0,0 +1,6 @@
+package com.example.arklens.models
+
+data class Character(
+    val id: String,
+    val name: String
+)
